2836de1ac6db50dd2121cdc0ddbbd0b4d9457a97
Cheong <cheongsole@gmail.com>
2020-02-17 13:34:18 +0800

    init
    init

---
+++ src/handler.lua
@@ -0,0 +1,34 @@
+local BasePlugin = require "kong.plugins.base_plugin"
+local http = require "resty.http"
+
+local ExternalAuthHandler = BasePlugin:extend()
+
+function ExternalAuthHandler:new()
+    ExternalAuthHandler.super.new(self, "external-auth")
+end
+
+function ExternalAuthHandler:access(conf)
+    ExternalAuthHandler.super.access(self)
+
+    local client = http.new()
+    client:set_timeouts(conf.connect_timeout, send_timeout, read_timeout)
+
+    local res, err = client:request_uri(conf.url, {
+        method = kong.request.get_method(),
+        headers = kong.request.get_headers()
+
+    })
+
+    if not res then
+        return kong.response.exit(500, { message = err })
+    end
+
+    if res.status ~= 200 then
+        return kong.response.exit(401, { message = "Invalid authentication credentials" })
+    end
+end
+
+ExternalAuthHandler.PRIORITY = 900
+ExternalAuthHandler.VERSION = "0.1.0"
+
+return ExternalAuthHandler
